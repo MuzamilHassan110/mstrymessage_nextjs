324689f22a337573e50c03fa0ddd803912c0c2e1
MuzamilHassan110 <150364484+MuzamilHassan110@users.noreply.github.com>
2025-01-16 16:33:38 +0500

    resend eamil

---
+++ src/app/helpers/sendEmailVerification.tsx
@@ -0,0 +1,25 @@
+import { resend } from "../lib/resendEmail";
+import { ApiResponse } from "../types/apiResponse";
+import  VerificationEmai  from "../../../emails/EmailVerification";
+
+
+const sendEmailVerification = async (
+    username: string,
+    email: string,
+    verifyCode: string
+): Promise<ApiResponse> => {
+    try {
+        const { data, error } = await resend.emails.send({
+            from: 'Acme <onboarding@resend.dev>',
+            to: email,
+            subject: 'Email Verification Code',
+            react: <VerificationEmai  username = {username} otp = {verifyCode}/>,
+          });
+       
+        return { message: "Email verification sent successfully", success: true };
+
+    } catch (error) {
+        console.error("Error sending email verification", error);
+        return { message: "Error sending email verification", success: false };
+    }
+}
