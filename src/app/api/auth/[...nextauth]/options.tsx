19ba02060db6807c54862d57e31dfe2df19d006d
MuzamilHassan110 <150364484+MuzamilHassan110@users.noreply.github.com>
2025-01-17 12:51:07 +0500

    implement the next-auth

---
+++ src/app/api/auth/[...nextauth]/options.tsx
@@ -0,0 +1,79 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import dbConnection from "@/app/lib/dbConnection";
+import UserModal from "@/app/models/user.modal";
+import bcrypt from "bcrypt";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials: any): Promise<any> {
+        await dbConnection();
+        try {
+          const user = await UserModal.findOne({
+            $or: [
+              { email: credentials.identifier },
+              { username: credentials.identifier },
+            ],
+          });
+          if (!user) {
+            throw new Error("User can't found");
+          }
+          if (!user.isVerified) {
+            throw new Error("Please verifed your email before login");
+          }
+
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+          if (!isPasswordCorrect) {
+            throw new Error("Password is not correct");
+          } else {
+            return user;
+          }
+        } catch (error: any) {
+          throw new Error(error);
+        }
+      },
+    }),
+
+  ],
+  pages: {
+    signIn: '/sing-in',
+  },
+  session: {
+      strategy: "jwt"
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+
+  // Here Next Auth work with Session and so we put the max data inside the session object
+  // and also avoid the again and again calls of the database
+  
+  callbacks: {
+async session({ session, token }) {
+        if(token){
+          session.user._id = token?._id;
+          session.user.isVerified = token?.isVerified;
+          session.user.isAcceptingMessages = token?.isAcceptingMessages;
+          session.user.username = token?.username;
+        }
+      return session
+    },
+    async jwt({ token, user }) {
+      if(user){
+        token._id = user._id?.toString();
+        token.isVerified = user.isVerified;
+        token.isAcceptingMessages = user.isAcceptingMessages;
+        token.username = user.username;
+      }
+      return token
+    }
+  }
+};
