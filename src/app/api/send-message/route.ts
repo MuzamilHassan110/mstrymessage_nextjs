b6bf37d73ec0dc483ddc97abd0d9100c49a33372
MuzamilHassan110 <150364484+MuzamilHassan110@users.noreply.github.com>
2025-01-20 14:44:58 +0500

    create send-message route

---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,28 @@
+import UserModal from "@/app/models/user.modal";
+import dbConnection from "@/app/lib/dbConnection";
+import { messageSchema } from "@/app/schemas/messageSchema";
+import { Message } from "@/app/models/user.modal";
+import { ResponseHelper } from "@/app/helpers/ResponseHelper";
+
+export async function POST(req: Request) {
+  await dbConnection();
+  const { username, content } = await req.json();
+
+  try {
+    const user = await UserModal.findOne({ username });
+    if (!user) {
+      return ResponseHelper.jsonResponse("User not found", 404);
+    }
+    // User Accepting the messages
+    if (!user.isAcceptingMessage) {
+      return ResponseHelper.jsonResponse("User is not accepting messages", 403);
+    }
+    const newMessage = { content, createdAt: new Date() };
+    user.messages.push(newMessage as Message);
+    await user.save();
+    return ResponseHelper.jsonResponse("message send successfully", 200);
+  } catch (error) {
+    console.error("Error in sending message", error);
+    return ResponseHelper.jsonResponse("Error sending message", 500);
+  }
+}
